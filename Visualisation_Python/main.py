777ec120d7d822d4356980fc0624004c6bdc2bc8
samuel_buhan <samuelbuhan@gmailcom>
2022-05-06 22:47:37 +0200

    Update the visualisation tool and improve frame sent

--- Visualisation_Python/main.py
+++ Visualisation_Python/main.py
@@ -10,7 +10,13 @@ class IMUVisualizer(QtWidgets.QWidget):
     """
     Visualize IMU data on PyQtgraph
     """
-    nbData = 125
+    nbData = 14 # number of data per frame
+
+    # accelerometer sensitivity scale factor corresponding to {2g,4g,8g,16g}
+    accSensScaleFactor = {"2g":16384, 
+                          "4g" :8192,
+                          "8g" : 4096,
+                          "16g" : 2048}
     all_accX = []
     all_accY = []
     all_accZ = []
@@ -26,7 +32,7 @@ class IMUVisualizer(QtWidgets.QWidget):
             if self.serialPort.isOpen():
                 self.timer = QtCore.QTimer()
                 self.timer.timeout.connect(self.readFrame)
-                self.timer.start(10)
+                self.timer.start(1)
         except: 
             print("!COM port not found!")
             pass
@@ -68,24 +74,29 @@ class IMUVisualizer(QtWidgets.QWidget):
         self.plotItemMagnY = self.layoutGraphs.addPlot(title="MagnY")
         self.plotItemMagnZ = self.layoutGraphs.addPlot(title="MagnZ")
 
+        # set limits 
+        self.plotItemAccX.setYRange(-2,2)
+        self.plotItemAccY.setYRange(-2,2)
+        self.plotItemAccZ.setYRange(-2,2)
+
         self.GraphAccX = self.plotItemAccX.plot()
         self.GraphAccY = self.plotItemAccY.plot()
         self.GraphAccZ = self.plotItemAccZ.plot()
 
-        self.GraphAccX = self.plotItemGyroX.plot()
-        self.GraphAccY = self.plotItemGyroY.plot()
-        self.GraphAccZ = self.plotItemGyroZ.plot()
+        self.GraphGyroX = self.plotItemGyroX.plot()
+        self.GraphGyroY = self.plotItemGyroY.plot()
+        self.GraphGyroZ = self.plotItemGyroZ.plot()
 
-        self.GraphAccX = self.plotItemMagnX.plot()
-        self.GraphAccY = self.plotItemMagnY.plot()
-        self.GraphAccZ = self.plotItemMagnZ.plot()
+        self.GraphMagnX = self.plotItemMagnX.plot()
+        self.GraphMagnY = self.plotItemMagnY.plot()
+        self.GraphMagnZ = self.plotItemMagnZ.plot()
 
         TmpLayout = QtWidgets.QHBoxLayout()
         TmpLayout.addWidget(self.view)
         self.tabVisualise.setLayout(TmpLayout)
 
 
-
+        
 
 
 
@@ -93,23 +104,41 @@ class IMUVisualizer(QtWidgets.QWidget):
         # self.view.show()
     def readFrame(self):
         if (self.serialPort.inWaiting()):
-            frame = self.serialPort.read((self.serialPort.inWaiting()//self.nbData)*self.nbData)
+            frame = self.serialPort.read(self.nbData)
             if frame == b'': return
             elif frame[0] == 0x55:
+                # print("Frame:",frame,";len:",len(frame))
                 # print("Frame:",frame.hex('-'),";len:",len(frame))
-                for i in range(len(frame)//self.nbData):
-                    # print("subFrame:",frame[self.nbData*i:self.nbData*(i+1)].hex('-'),";len:",len(frame[self.nbData*i:self.nbData*(i+1)]))
-                    # print("Crc frame :",hex(frame[self.nbData*(i+1)-1]))
-                    # print("Crc check :",hex(self.crcCheck(frame[self.nbData*i:self.nbData*(i+1)])))
-                    if frame[self.nbData - 1] == self.crcCheck(frame[0:self.nbData]):
-                        for j in range(10):
-                            self.all_accX.append(frame[1+(j*2)] + (frame[2+(j*2)]<<8))
+                # print("CrcFrame:",hex(frame[self.nbData - 1]))
+                # print("subFrame:",frame[0:self.nbData - 1].hex('-'))
+                # print("CrcSubFrame:",hex(self.crcCheck(frame[0:self.nbData])))
+                # print('-' * 10)
+                if frame[self.nbData - 1] == self.crcCheck(frame[0:self.nbData]):
+                    for j in range(1):
+                        # print("Acc:",(frame[1+(j*2)]<<8) + frame[2+(j*2)])
+                        accX = ((frame[1+(j*2)]<<8) + frame[2+(j*2)])
+                        accX = (accX - 0x10000)  if (accX > 0x7FFF) else accX
 
-                self.GraphAccX.setData(self.all_accX)
+                        accY = ((frame[3+(j*2)]<<8) + frame[4+(j*2)])
+                        accY = (accY - 0x10000)  if (accY > 0x7FFF) else accY         
+
+                        accZ = ((frame[5+(j*2)]<<8) + frame[6+(j*2)]) 
+                        accZ = (accZ - 0x10000)  if (accZ > 0x7FFF) else accZ     
+
+                        self.all_accX.append(accX / self.accSensScaleFactor["2g"])
+                        self.all_accY.append(accZ / self.accSensScaleFactor["2g"])
+                        self.all_accZ.append(accZ / self.accSensScaleFactor["2g"])
+                # print("Acc:",self.all_accX[-1])
+                self.GraphAccX.setData(self.all_accX[-1000:])
+                self.GraphAccY.setData(self.all_accY[-1000:])
+                self.GraphAccZ.setData(self.all_accZ[-1000:])
 
     def crcCheck(self, frame):  
         crcValue = 0x00
-        for i in range(0,len(frame)-1):
+        # original frame : b'U\xf9\xe4\xf9\xa4\xf0'
+        # frame= b'U\xf9\xe4\xf9\xa4\xf0'
+        for i in range(0,len(frame) - 1):
+            # print("frame[",i,"]:",hex(frame[i]))
             crcValue = crcValue ^ frame[i]
         return crcValue
 
